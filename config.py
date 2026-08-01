b8487ce07b498354c3fda54c372364e6e3b0ad21
hello-botcmd <swaruppanda2008@gmail.com>
2026-08-02 02:23:53 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+API_ID = 22657083
+API_HASH = "d6186691704bd901bdab275ceaab88f3"
+BOT_TOKEN = "8866049309:AAH5DZGW91hm7I8ck2vWUy-bXDGt-Sxkxb0"
+
+# Source -> Destination
+CHANNEL_A = -1004428509253   # posts here get mirrored
+CHANNEL_B = -1003998331302   # mirrored here
+
+# REQUIRED for voice chat sync (bots can't manage VCs).
+# Generate it once with the command in the README section below.
+USER_SESSION = ""
